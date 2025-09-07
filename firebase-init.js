02d4338483c26d8fdb39187263d54990b0e2b1a2
Denis-2007 <95683398+Denis-2007@users.noreply.github.com>
2025-09-08 00:12:16 +0200

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,19 +1,19 @@
+// Import SDK Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
+// Configurazione Firebase (presa dal tuo file)
 const firebaseConfig = {
-  apiKey: "AIzaSyD2GGWWPn5P2xgw7YGWiJ5ZUXFRbHJkVy4",
-  authDomain: "bacheca-presonale.firebaseapp.com",
-  projectId: "bacheca-presonale",
-  storageBucket: "bacheca-presonale.firebasestorage.app",
-  messagingSenderId: "147412610802",
-  appId: "1:147412610802:web:b7cddfbf3a11c312576a6b",
-  measurementId: "G-QM12GWW70Y"
+  apiKey: "AIzaSyDplTt9uQ0V7HjKxB_HBaJjx5euMbvK9eA",
+  authDomain: "bacheca-firebase.firebaseapp.com",
+  projectId: "bacheca-firebase",
+  storageBucket: "bacheca-firebase.appspot.com",
+  messagingSenderId: "454108717508",
+  appId: "1:454108717508:web:3d2f67f0c7b40b2c7f6710"
 };
 
+// Inizializza Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db };
